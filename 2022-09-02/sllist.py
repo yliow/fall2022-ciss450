815520e989ccf2d440f45f82fc63e4965135f8e2
Yihsiang Liow <yliow@ccis.edu>
2022-09-02 17:58:28 -0400

    no msg

--- 2022-09-02/sllist.py
+++ 2022-09-02/sllist.py
@@ -14,6 +14,16 @@ class SLList:
         self.__head = None        
     def insert_head(self, key):
         self.__head = SLNode(key, self.__head)
+    def delete_head(self):
+        """ No underflow exception thrown """
+        if self.__head != None:
+            self.__head = self.__head.next_node
+            # NOTE: There's no mem leak because of garbage collector
+    def get_head_key(self):
+        """ No underflow exeption thrown """
+        if self.__head != None:
+            return self.__head.key
+    head = property(get_head_key, None)
     def __str__(self):
         node = self.__head
         xs = []
@@ -29,3 +39,16 @@ sllist = SLList()
 for x in [42,0,1,6]:
     sllist.insert_head(x)
     print(sllist)
+
+for x in range(10):
+    head = sllist.head
+    sllist.delete_head()
+    print(head, sllist)
+
+# To check for memory leaks, run this program and in another shell run
+# 'top' and check the %MEM for memory usage. If there's a memory leak the
+# value of %MEM will grow.
+while 1:
+    sllist = SLList()
+    for x in range(1000):
+        sllist.insert_head(x)
