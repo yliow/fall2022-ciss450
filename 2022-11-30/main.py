6e197cae96fd863bd9033b09a311c59ff2599650
Yihsiang Liow <yliow@ccis.edu>
2022-12-02 19:22:31 -0500

    no msg

--- 2022-11-30/main.py
+++ 2022-11-30/main.py
@@ -3,7 +3,7 @@ import math
 def S(z):
     return 1.0 / (1.0 + math.exp(-z))
 
-def f(z):
+def f(z): # treshold function
     if z <= 0.5:
         return 0.0
     else:
@@ -27,8 +27,49 @@ class Neuron:
         return self.A(s)
 
 
-AND = Neuron([0.9, 0.1, 0.1], A)
-print(AND(1, 1))
-print(AND(1, 0))
-print(AND(0, 1))
-print(AND(0, 0))
+def test(testcases):
+    for w0 in range(-10, 11):
+        w0 = w0 / 10.0
+        for w1 in range(-10, 11):
+            w1 = w1 / 10.0
+            for w2 in range(-10, 11):
+                w2 = w2 / 10.0
+                
+                n = Neuron([w0, w1, w2], A)
+                found = True
+                for x1, x2, correct in testcases:
+                    if n(x1, x2) != correct:
+                        found = False
+                        break
+                if found:
+                    return w0, w1, w2
+    return None
+
+print("computing AND model")
+w0, w1, w2 = test([(0,0,0),
+                   (0,1,0),
+                   (1,0,0),
+                   (1,1,1),
+    ])
+print(w0, w1, w2)
+AND = Neuron([w0, w1, w2], A)
+
+print("computing OR model")
+w0, w1, w2 = test([(0,0,0),
+                   (0,1,1),
+                   (1,0,1),
+                   (1,1,1),
+    ])
+print(w0, w1, w2)
+OR = Neuron([w0, w1, w2], A)
+
+
+print("computing XOR model")
+ret = test([(0,0,0),
+            (0,1,1),
+            (1,0,1),
+            (1,1,0),
+    ])
+print(ret)
+#print(w0, w1, w2)
+#OR = Neuron
